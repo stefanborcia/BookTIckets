6f874423541e755682fc4fc662e381df1b15e3eb
Stefan <stefigabi89@yahoo.com>
2024-06-04 14:29:35 +0200

    install npm font awesome/bootstraap and create a search-flight component

--- booktickets.client/src/app/app.module.ts
+++ booktickets.client/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchFlightsComponent } from './search-flights/search-flights.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchFlightsComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
