f3aa9421b9323795ac103935f7d154362ea7bf46
Stefan <stefigabi89@yahoo.com>
2024-06-06 11:12:58 +0200

    Fetching and Displaying Flight Data and add a new angular component navigation

--- booktickets.client/src/app/app.module.ts
+++ booktickets.client/src/app/app.module.ts
@@ -1,18 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { SearchFlightsComponent } from './search-flights/search-flights.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SearchFlightsComponent,
+    NavMenuComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '', component: SearchFlightsComponent
+        , pathMatch: 'full'
+      },
+      { path: 'search-flights', component: SearchFlightsComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
